2cc563a5331ceda6ac3bf6ae0fd306b2a6389e15
Bence Lovász <lovasz.b99@gmail.com>
2024-11-28 21:43:23 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: ':groupId',
+    loadComponent: () => import('./chat/chat.component')
+      .then(c => c.ChatComponent),
+  }
+];
